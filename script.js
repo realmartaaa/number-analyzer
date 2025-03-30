4a71cc4a41ebe4288dd9d2daef7cbd29314f6428
martadzneladze <martadzneladze20@gmail.com>
2025-03-30 17:00:33 +0400

    third commit

--- script.js
+++ script.js
@@ -6,23 +6,3 @@ function isPrime(num) {
 console.log(isPrime(5))
 console.log(isPrime(11))
 
-function isEven(num) {
-    if (num % 2 === 0) return true
-    else return false
-}
-
-console.log(isEven(52))
-console.log(isEven(11))
-
-function sumDigits(num) {
-    let digits = String(num).split('')
-    let sum = 0
-
-    for (let digits of digits) {
-        sum += Number(digits)
-    }
-    return sum
-}
-
-console.log(sumDigits(24))
-console.log(sumDigits(11))
