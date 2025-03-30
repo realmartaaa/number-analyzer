3dec815b145baf99a95a8bbc94faf1f5822ec343
martadzneladze <martadzneladze20@gmail.com>
2025-03-30 17:46:56 +0400

    fourth commit

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ function isPrime(num) {
 console.log(isPrime(5))
 console.log(isPrime(11))
 
+function isEven(num) {
+    if (num % 2 === 0) return true
+    else return false
+}
+
+console.log(isEven(52))
+console.log(isEven(11))
